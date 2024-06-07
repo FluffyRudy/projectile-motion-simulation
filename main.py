323c7744eec9fc4b9a8263fbce669d6641fceb7b
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-06-07 12:15:34 +0545

    initial setup

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+
+class Simulation:
+    def __init__(self):
+        pass
+
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(pos: tuple[int, int], radius: int):
+        super().__init__(self)
+        self.image = pygame.Surface
