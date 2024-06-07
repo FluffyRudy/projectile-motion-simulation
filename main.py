3eec287539172c84b8fc5423e2efb88902005594
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-06-07 20:36:54 +0545

    add input field and remove hardcoded colors

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from random import randint, choice
 from math import sin, cos, atan2, radians, degrees, hypot
 
 SCREEN_COLOR = (100, 100, 100)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 
 class Simulation:
@@ -24,6 +26,17 @@ class Simulation:
 
         self.origin_pos = Vector2(0, self.limit_bound[1] - 200)
 
+        # text input
+        self.input_field = pygame.Surface((250, 50), pygame.SRCALPHA)
+        self.input_field.fill(BLACK)
+        self.input_field_rect = self.input_field.get_rect(
+            topright=(self.limit_bound[0], 0)
+        )
+        self.input_active = False
+
+        self.velocity_text = ""
+        self.velocity = 0
+
     def handle_event(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -38,26 +51,68 @@ class Simulation:
                 self.angle = direction * degrees(
                     atan2(vertical_distance, horizontal_distance)
                 )
-
-            if event.type == pygame.MOUSEBUTTONUP:
-                ball = Ball(self.origin_pos, self.BALL_RADIUS, 60, self.angle)
+            if event.type == pygame.MOUSEBUTTONUP and not self.input_active:
+                ball = Ball(
+                    self.origin_pos,
+                    self.BALL_RADIUS,
+                    self.velocity,
+                    self.angle,
+                )
                 self.ball_group.add(ball)
 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if self.input_field_rect.collidepoint(event.pos):
+                    self.input_active = True
+                    self.velocity_text = ""
+                    self.input_field.fill(BLACK)
+                else:
+                    self.input_active = False
+                    self.velocity = int(self.velocity_text) if self.velocity_text else 0
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    self.input_active = False
+                    if self.velocity_text:
+                        try:
+                            self.velocity = int(self.velocity_text)
+                        except ValueError:
+                            print("Invalid velocity input!")
+                    else:
+                        print("Please enter a velocity value!")
+
+                if event.key == pygame.K_BACKSPACE:
+                    self.velocity_text = self.velocity_text[:-1]
+
+                if str(event.unicode).isnumeric() and self.input_active:
+                    if self.velocity_text == "0":
+                        self.velocity_text = ""
+                    self.velocity_text += str(event.unicode)
+
+    def text_input(self, char: str):
+        pass
+
     def draw_line(self):
-        pygame.draw.line(self.screen, "white", self.origin_pos, pygame.mouse.get_pos())
+        pygame.draw.line(self.screen, WHITE, self.origin_pos, pygame.mouse.get_pos())
 
     def run(self):
         while True:
             self.delta = self.clock.tick(self.FPS)
             self.handle_event()
             self.screen.fill(SCREEN_COLOR)
+            self.input_field.fill(BLACK)
 
-            angle = self.font.render(f"{self.angle}", True, "white")
+            angle = self.font.render(f"{self.angle}", True, WHITE)
             self.screen.blit(angle, (0, 0))
+            self.input_field.blit(
+                self.font.render(self.velocity_text, True, WHITE), (0, 0)
+            )
 
-            self.draw_line()
+            if not self.input_active:
+                self.draw_line()
             self.ball_group.update(self.screen, self.limit_bound, self.delta)
             self.ball_group.draw(self.screen)
+
+            self.screen.blit(self.input_field, self.input_field_rect.topleft)
 
             pygame.display.update()
 
@@ -75,7 +130,7 @@ class Ball(pygame.sprite.Sprite):
         self.direction = 1
         self.radius = radius
         self.speed = speed
-        self.angle = math.radians(degree)
+        self.angle = radians(degree)
         self.time = 0
         self.color = get_random_rgb()
 
@@ -90,10 +145,9 @@ class Ball(pygame.sprite.Sprite):
 
         self.time += delta / 100.0
         self.rect.y = self.initial_position.y - (
-            self.speed * math.sin(self.angle) * self.time
-            - 0.5 * self.GRAVITY * self.time**2
+            self.speed * sin(self.angle) * self.time - 0.5 * self.GRAVITY * self.time**2
         )
-        self.rect.x += self.direction * self.speed * math.cos(self.angle)
+        self.rect.x += self.direction * self.speed * cos(self.angle)
 
         pygame.draw.circle(
             self.image, self.color, (self.radius, self.radius), self.radius
